51126956ff2dca9151137d1d2349e02ac5df8497
Enrique Gonzalez <Enrique@Enriques-MacBook-Pro.local>
2013-09-26 13:30:10 -0600

    Started new Facenovel project: A Facebook clone using Ruby on Rails

---
+++ Facenovel/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Facenovel::Application.config.secret_key_base = 'a312eb418f1fc36fd4b5210320b07b2fb9b4232975996c6c150d1b8869bb2a0ff7e4c72fd12802e2bc1a731694b089804c4a256677251947b4b9c4b4b4437d54'
